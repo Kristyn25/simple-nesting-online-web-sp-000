a96ba529364e434882af8e972d5e8d10fecb55d7
Kristyn Little <kristynlittle@frontier.com>
2021-03-29 02:40:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_richie][:languages]<<"Assembly"
-		 programmer_hash
+	programmer_hash[:dennis_richie][:languages]<<"Assembly"
+	programmer_hash
 end
+
+adding_to_dennis
